b255bf477de8a6d32a3bc78b982e419c0898f58e
kondylisjg <45542617+kondylisjg@users.noreply.github.com>
2018-12-22 12:12:58 -0800

    Update get_token_example.py

--- get_token_example.py
+++ get_token_example.py
@@ -4,8 +4,6 @@ import getpass
 from ConfigParser import ConfigParser
 def gen_token():
     web_service_url = 'https://ndar.nih.gov/DataManager/dataManager'
-    #username  = input('Enter your NIMH Data Archives username:')
-    #password  = getpass.getpass('Enter your NIMH Data Archives password:')
 
     generator = NDATokenGenerator(web_service_url)
     username='inset username here'
@@ -14,7 +12,7 @@ def gen_token():
 
     # Read .aws/credentials from the user's HOME directory, add a NDA profile, and update with credentials
     parser = ConfigParser()
-    parser.read(os.path.expanduser('/home/kondylisjg/.aws/credentials'))
+    parser.read(os.path.expanduser('/insert/path/to/.aws/credentials'))
 
     if not parser.has_section('NDA'):
         parser.add_section('NDA')
@@ -22,7 +20,7 @@ def gen_token():
     parser.set('default', 'aws_secret_access_key', token.secret_key)
     parser.set('default', 'aws_session_token', token.session)
 
-    with open (os.path.expanduser('/home/kondylisjg/.aws/credentials'), 'wb') as configfile:
+    with open (os.path.expanduser('/insert/path/to/.aws/credentials'), 'wb') as configfile:
         parser.write(configfile)
     '''
     print('aws_access_key_id=%s\n'
